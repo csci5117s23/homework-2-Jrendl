0951d236510bd8c3a5dd18a4f09876a9cb0cf353
Joe <rendl008@umn.edu>
2023-04-06 16:39:11 -0500

    checkbox won't flip

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,20 @@
-
 /*
 * Auto generated Codehooks (c) example
 * Install: npm i codehooks-js codehooks-crudlify
 */
 import {app} from 'codehooks-js'
 import {crudlify} from 'codehooks-crudlify'
+import {object, number, string, boolean} from 'yup'
+
+const todosSchema = object({
+  _id: number().positive().integer().required(),
+  description: string().required(),
+  user_id: string().required(),
+  done: boolean().required()
+}
+
+)
+
 
 // test route for https://<PROJECTID>.api.codehooks.io/dev/
 app.get('/', (req, res) => {
@@ -12,7 +22,7 @@ app.get('/', (req, res) => {
 })
 
 // Use Crudlify to create a REST API for any collection
-crudlify(app)
+crudlify(app, {todos: todosSchema})
 
 // bind to serverless runtime
 export default app.init();
